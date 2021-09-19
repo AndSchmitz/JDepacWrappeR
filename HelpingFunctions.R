41197c6dd934854e45950ffbc4b3ce4d4b764d37
Andreas Schmitz <47260300+AndSchmitz@users.noreply.github.com>
2021-09-19 19:05:34 +0200

    Major changes to work with JDepac version 0.3.0
    See Readme section "Update history".

--- HelpingFunctions.R
+++ HelpingFunctions.R
@@ -3,105 +3,71 @@
 #
 
 
-
 #-------------------------------------------------------------------------
-#Function to convert the output of one jDepac run into one dataframe row
+#Function to convert the output of one JDepac run into one dataframe row
 ParsejDepacResponse <- function(jDepacResponse) {
   
-  #Row 1 of jDepac output: Version
-  Version <- data.frame(
-    version = jDepacResponse[1]
+  #Relevant rows of JDepac response start with INP, CLC or OUT
+  CurrentOutput <- grep(
+    x = jDepacResponse,
+    pattern = "INP;|CLC;|OUT;",
+    value = T
   )
   
-  #Row 2...n of jDepac output
-  #Each row consists of an identifier (CRA, CRB, CRC, VDP) followed by whitespace and numer of key=value pairs
-  ListOfParsedResponses <- list()
-  for ( i in 2:length(jDepacResponse) ) {
-    
-    #Skip unexpected output rows:
-    ValidLineStarts <- c("CRA", "CRB", "CRC", "VDP")
-    CurrentLineStart <- substr(x =jDepacResponse[i], start = 1, stop = 3)
-    if ( !(CurrentLineStart %in% ValidLineStarts) ) {
-      next
-    }
-    
-    CurrentResponseComponent <- unlist(strsplit(
-      x = jDepacResponse[i],
-      split = " "
-    ))
-    if ( length(CurrentResponseComponent) == 0 ) {
-      #Skip if response line is empty string
-      next
-    }
-    #Parse current component
-    CurrentComponentParsed <- ParsejDepacResponseComponent(CurrentResponseComponent)
-    #Append CurrentComponentParsed to list of results
-    ListOfParsedResponses[[i]] <- CurrentComponentParsed
-    
-  } #end of loop over jDepac output lines
+  #Helping function to split key-value paired JDepac output
+  #This function is applied to each item of the JDepac output
+  SplitKeyValuePair <- function(OnejDepacResponseItem) {
   
-  #Drop attributes and convert factor levels to character
-  ReturnValue <- bind_cols(Version,ListOfParsedResponses)
-  ReturnValue[] <- lapply(ReturnValue, unname)
-  ReturnValue[] <- lapply(ReturnValue, as.character)
+    # OnejDepacResponseItem <- CurrentOutput[1]
+    ItemName <- str_extract(
+      string = OnejDepacResponseItem,
+      pattern = "^.*\\("
+    )
+    ItemName <- str_replace(
+      string = ItemName,
+      pattern = ";",
+      replacement = ""
+    )
+    ItemName <- str_replace(
+      string = ItemName,
+      pattern = "\\(",
+      replacement = ""
+    )
+    ItemName <- str_replace(
+      string = ItemName,
+      pattern = " ",
+      replacement = "_"
+    )
+    ItemValue <- str_split(
+      string = OnejDepacResponseItem,
+      pattern = ";"
+    )[[1]][3]
+    ItemValue <- str_replace(
+      string = ItemValue,
+      pattern = "\\s*",
+      replacement = ""
+    )
+    
+    #Return current item as a one-row one-col data.frame
+    DF <- as.data.frame(ItemValue)
+    colnames(DF) <- ItemName
+    return(DF)
+    
+  } #end of helping functions
+  
+  #Apply helping function to current JDepac output row
+  OutputRowAsDataFrame <- lapply(
+    X = CurrentOutput,
+    FUN = SplitKeyValuePair
+  )
+  #And bind all the one-row one-col data.frames of current JDepac output row
+  #to a one-row n-column data.frame
+  OutputRowAsDataFrame <- bind_cols(OutputRowAsDataFrame)
   
   #Return
-  return(ReturnValue)
+  return(OutputRowAsDataFrame)
 }
 
-
-
-#-------------------------------------------------------------------------
-#Function to parse a single jDepac output component (INP, CRA, CLC, RES)
-ParsejDepacResponseComponent <- function(jDepacResponseComponent) {
-  
-  Components <- unlist(strsplit(
-    x = jDepacResponseComponent,
-    split = " "
-  ))
-  #Prepend identifier (first element: CRA, CRB, etc.) to all other elements
-  #to guarantee unique unique column names
-  Identifier <- Components[1]
-  Components <- Components[-1]
-  Components <- paste0(Identifier,"_",Components)
-  #Extract data from key=value pairs
-  Keys <- vector()
-  Values <- vector()
-  for ( i in 1:length(Components) ) {
-    CurrentKeyValuePair <- SplitKeyValuePair(Components[i])
-    Keys <- as.character(c(Keys,CurrentKeyValuePair["Key"]))
-    Values <- as.character(c(Values,CurrentKeyValuePair["Value"]))
-  }
-  
-  ParsedComponent <- Values
-  names(ParsedComponent) <- Keys
-  ParsedComponent <- as.data.frame(t(ParsedComponent))
-  return(ParsedComponent)
-  
-}
-
-
-
-#-------------------------------------------------------------------------
-#Function to split a "key=value" pair into a named vector
-SplitKeyValuePair <- function(KeyValuePair, sep = "=") {
-  nOccurrencesSepChar <- str_count(
-    string = KeyValuePair,
-    pattern = sep
-  )
-  if ( nOccurrencesSepChar != 1 ) {
-    stop(paste0("Error in SplitKeyValuePair(): Separator \"",sep,"\" occurs zero or multiple times in string \"",KeyValuePair,"\""))
-  }
-  Key = unlist(strsplit(
-    x = KeyValuePair,
-    split = sep
-  ))[[1]]
-  Value = unlist(strsplit(
-    x = KeyValuePair,
-    split = sep
-  ))[[2]]
-  return(c(Key = Key, Value = Value))
-}
 
 
 
@@ -111,126 +77,32 @@ ConvertCSVInputRowTojDepacInput <- function(OneCSVInputLine) {
   if ( nrow(OneCSVInputLine) != 1 ) {
     stop("Function ConvertInputCSVTojDepac() must be called with a 1-row dataframe.")
   }
-  jDepacInputString <- ""
+  jDepacInputString <- as.vector(OneCSVInputLine)
+  CurrentID <- jDepacInputString$ID
   
-  #Treat special columns: -o background concentrations
-  BackgroundConcentrations <- paste0(
-    "-o",
-    OneCSVInputLine[1,"CmpConcentration"],
-    ",",
-    OneCSVInputLine[1,"BackgroundNH3"],
-    ",",
-    OneCSVInputLine[1,"BackgroundSO2"],
-    " "
-  )
-  nMissing <- str_count(
-    string = BackgroundConcentrations,
-    pattern = "NA"
-  )
-  #If all three parameter values required for option -o are present,
-  #append to jDepac input
-  if ( nMissing == 0 ) {
-    jDepacInputString <- paste0(jDepacInputString,BackgroundConcentrations)
-    #if none of three is provided - ignore parameter
-  } else if ( nMissing == 3 ) {
-    #Else stop
-  } else {
-    stop("Values for either all or none of the columns CmpConcentration, BackgroundNH3, BackgroundSO2 must be provided.")
+  #Drop arguments with NA values
+  jDepacInputString <- jDepacInputString[names(jDepacInputString) != "ID"]
+  ArgNames <- paste0("--",names(jDepacInputString))
+  idx_drop <- which(is.na(jDepacInputString))
+  if ( length(idx_drop) > 0 ) {
+    jDepacInputString <- jDepacInputString[-idx_drop]
+    ArgNames <- ArgNames[-idx_drop]
+  }
+  if ( length(jDepacInputString) == 0 ) {
+    stop(paste("All parameters are NA for input ID",CurrentID))
   }
   
-  #Treat special columns: -U wind speed
-  WindSpeed <- paste0(
-    "-U",
-    OneCSVInputLine[1,"ua"],
-    ",",
-    OneCSVInputLine[1,"ha"],
-    " "
-  )
-  nMissing <- str_count(
-    string = WindSpeed,
-    pattern = "NA"
-  )  
-  #If both parameter values required for option -U are present,
-  #append to jDepac input
-  if ( nMissing == 0 ) {
-    jDepacInputString <- paste0(jDepacInputString,WindSpeed)
-    #if none of two is provided - ignore parameter
-  } else if ( nMissing == 2 ) {
-    #Else stop
-  } else {
-    stop("Values for either all or none of the columns ua and ha must be provided.")
-  }  
-  
-  #Treat special columns: -Z Surface roughness
-  Roughness <- paste0(
-    "-Z",
-    OneCSVInputLine[1,"z0"],
-    ",",
-    OneCSVInputLine[1,"d0"],
-    " "
-  )
-  nMissing <- str_count(
-    string = Roughness,
-    pattern = "NA"
-  )    
-  #If both parameter values required for option -Z are present,
-  #append to jDepac input
-  if ( nMissing == 0 ) {
-    jDepacInputString <- paste0(jDepacInputString,Roughness)
-    #if none of two is provided - ignore parameter
-  } else if ( nMissing == 2 ) {
-    #Else stop
-  } else {
-    stop("Values for either all or none of the columns z0 and d0 must be provided.")
-  }   
-  
-  #Treat special columns: -D Aerosol size
-  AerosolSize <- paste0(
-    "-D",
-    OneCSVInputLine[1,"dae"],
-    ",",
-    OneCSVInputLine[1,"rho"],
-    " "
-  )
-  nMissing <- str_count(
-    string = AerosolSize,
-    pattern = "NA"
-  )
-  #If both parameter values required for option -D are present,
-  #append to jDepac input
-  if ( nMissing == 0 ) {
-    jDepacInputString <- paste0(jDepacInputString,AerosolSize)
-    #if none of two is provided - ignore parameter
-  } else if ( nMissing == 2 ) {
-    #Else stop
-  } else {
-    stop("Values for either all or none of the columns dae and rho must be provided.")
-  }   
-  
-  #Delete special columsn from CSV
-  SpecialColumns <- c("CmpConcentration","BackgroundNH3","BackgroundSO2","ua","ha","z0","d0","dae","rho")
-  CurrentCSVInputLine <- CurrentCSVInputLine[!(colnames(CurrentCSVInputLine) %in% SpecialColumns)]
-  
-  #Append remaining columns one by one to jDepacInputString
-  for ( iCol in 1:ncol(CurrentCSVInputLine) ) {
-    CurrentColName <- colnames(CurrentCSVInputLine)[iCol]
-    CurrentValue <- CurrentCSVInputLine[1,CurrentColName]
-    
-    #Skip ID column
-    if ( CurrentColName == "ID" ) {
-      next
-    }
-    
-    #Skip empty input lines
-    if ( is.na(CurrentValue) ) {
-      next
-    }
-    
-    #Identify the jDepac parameter name corresponding to current CSV column name
-    CurrentParameterName <- InputTranslation$jDepacParameterName[InputTranslation$CSVColumnName == CurrentColName]
-    
-    #Append current parameter to jDepac input string
-    jDepacInputString <- paste0(jDepacInputString," -",CurrentParameterName,CurrentValue)
+  #Adjust name of column "nh3so2" for JDepac (add hyphen)
+  idx_nh3so2 <- which(ArgNames == "--nh3so2")
+  if ( length(idx_nh3so2) == 1 ) {
+    ArgNames[idx_nh3so2] <- "--nh3-so2"
   }
+  
+  #Concatenate JDepac input string
+  jDepacInputString <- paste(ArgNames, jDepacInputString, sep = "=", collapse = " ")
+  
+  #Return
   return(jDepacInputString)
+  
 }
+
--- jDepacWrappeR.R
+++ jDepacWrappeR.R
@@ -16,15 +16,16 @@ library(stringr) #string handling
 
 
 #Set working directory, use \\ or /
-WorkDir <- "/path/to/your/WorkDir"
+WorkDir <- "/Path/To/WorkDir"
 
-#jDepac JAR file name (must be located in WorkDir)
-jDepacJAR <- "JDepac-0.2.1.jar"
+#Name of JDepac JAR file (must be located in WorkDir)
+jDepacJAR <- "JDepac-0.3.0.jar"
 
+#Name of input file, expected in subfolder "Input" of WorkDir
+InputCSVName <- "DemoInput.csv"
 
 
 # --- No changes required below this line ---
-
 
 
 #Preparations----
@@ -36,95 +37,24 @@ InDir <- file.path(WorkDir,"Input")
 OutDir <- file.path(WorkDir,"Output")
 #Create output directory if not existing
 dir.create(OutDir,showWarnings = F)
+#Name for output file
+BatchOutputFileName <- file.path(OutDir,paste0("BatchRun-",format(Sys.time(),format = "%Y-%m-%d-%H-%M-%S"),".csv"))
 
 #Read input CSV-----
 #Must be located in folder "Input" in WorkDir
 CSVInput <- read.table(
-  file = file.path(InDir,"DemoInput.csv"),
+  file = file.path(InDir,InputCSVName),
   sep = ";",
   header = T,
   stringsAsFactors = F
 )
-
-
-#Read input translation table
-#Must be located in folder "Input" in WorkDir
-InputTranslation <- read.table(
-  file = file.path(InDir,"InputMapping.csv"),
-  sep = ";",
-  header = T,
-  stringsAsFactors = F
-)
-
-
-
-# --- No changes required below this line ---
-BatchOutputFileName <- file.path(OutDir,paste0("BatchRun-",format(Sys.time(),format = "%Y-%m-%d-%H-%M-%S"),".csv"))
-
 
 #Check CSV input----
 #Minimal check of CSV input validity
 CSVCols <- colnames(CSVInput)
 CSVCols <- CSVCols[CSVCols != "ID"]
-WrongInputCols <- CSVCols[!(CSVCols %in% InputTranslation$CSVColumnName)]
-if ( length(WrongInputCols) > 0 ) {
-  stop(paste("The following columns in input CSV do not correspond to valid columns as defined in InputMapping.csv:",paste(WrongInputCols,collapse = ",")))
-}
 #Convert empty string to NA, required later
 CSVInput[!is.na(CSVInput) & (CSVInput == "")] <- NA
-
-
-#Guess output table structure------
-#The number of output elements differs for different values in JDepac input parameter "cmp",
-#e.g. between particles, HNO3 and NH3. In order to guarantee a consistent column structure in the
-#output CSV for potentially mixed occurrences of different substances, all ocurring output
-#columns are determined in advance. This is done by running the first occurrence (line) of each
-#substance in the input CSV through jDepac and aggregating all occurring output elements/columns.
-CSVInput_FirstOccurrenceEachSubstance <- CSVInput %>%
-  group_by(cmp) %>%
-  mutate(
-    n = 1:n()
-  ) %>%
-  ungroup() %>%
-  filter(
-    n == 1
-  )
-OutputCols <- vector() #Vector with all output column names that will occur
-for ( iTestInput in 1:nrow(CSVInput_FirstOccurrenceEachSubstance) ) {
-  CurrentCSVInputLine <- CSVInput_FirstOccurrenceEachSubstance[iTestInput,]
-  CurrentjDepacInput <- ConvertCSVInputRowTojDepacInput(CurrentCSVInputLine)
-  #Running jDepac with current parametrization
-  jDepacResponse <- system(
-    command = paste0("java -jar \"", file.path(WorkDir,jDepacJAR),"\" ",CurrentjDepacInput),
-    intern = T
-  )
-  #Catch some errors
-  if ( any(grepl(x = jDepacResponse, pattern = "usage")) ) {
-    print(jDepacResponse)
-    stop("Error - stopping")
-  }
-  if ( any(grepl(x = jDepacResponse, pattern = "Exception")) ) {
-    print(jDepacResponse)
-    stop("Error - stopping")
-  }
-  #Convert jDepacResponse to data.frame
-  jDepacResponseDF <- ParsejDepacResponse(jDepacResponse)
-  OutputCols <- unique(c(OutputCols, colnames(jDepacResponseDF)))
-}
-#Create dumm dataframe with all expected columns
-DummyDF <- data.frame(
-  matrix(
-    data = rep(NA, length(OutputCols)),
-    ncol = length(OutputCols)
-  )
-)
-colnames(DummyDF) <- OutputCols
-DummyDF <- DummyDF %>%
-  mutate(
-    ID = NA
-  ) %>%
-  select(ID, version, everything())
-
 
 #Run batch-----
 #Empty list for batch results
@@ -146,11 +76,11 @@ for ( iCSVInput in 1:nrow(CSVInput) ) {
   #Catch some errors
   if ( any(grepl(x = jDepacResponse, pattern = "usage")) ) {
     print(jDepacResponse)
-    stop("Error - stopping")
+    stop(paste("Error with ID",CurrentID," - stopping."))
   }
   if ( any(grepl(x = jDepacResponse, pattern = "Exception")) ) {
     print(jDepacResponse)
-    stop("Error - stopping")
+    stop(paste("Error with ID",CurrentID," - stopping."))
   }
   
   #Convert jDepacResponse to data.frame
@@ -164,15 +94,8 @@ for ( iCSVInput in 1:nrow(CSVInput) ) {
     jDepacResponseDF
   )
   
-  #Make sure all columns are present (potentially with NA values)
-  NewOutputRow <- bind_rows(
-    DummyDF,
-    NewOutputRow
-  )
-  #Drop first row (NA) from DummyDF
-  NewOutputRow <- NewOutputRow[-1,]
-  
   #Convert UNDEF to NA to have columns of type numeric
+  #From version <0.3.0. Still relevant?
   NewOutputRow[NewOutputRow == "UNDEF"] <- NA
   
   #Append NewOutputRow row to batch output
@@ -212,3 +135,7 @@ for ( iCSVInput in 1:nrow(CSVInput) ) {
   }
 
 } #End of loop over input CSV rows
+
+#Finish-----
+print("jDepacWrappeR finished.")
+
